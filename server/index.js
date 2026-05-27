1a849f159918da14cde168c4d45282c632cee96a
Shweta Singh <20singhshweta@gmail.com>
2026-05-28 00:35:16 +0530

    Added MongoDB user model and database save functionality

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 require("dotenv").config()
 
+const User = require("./models/User")
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+// console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected")
@@ -20,12 +23,38 @@ app.get("/", (req, res) => {
   res.send("Backend is running")
 })
 
-app.post("/api/login", (req, res) => {
-  console.log(req.body)
+// app.post("/api/login", (req, res) => {
+//   console.log(req.body)
 
-  res.json({
-    message: "Login data received"
-  })
+//   res.json({
+//     message: "Login data received"
+//   })
+// })
+app.post("/api/login", async (req, res) => {
+
+  try {
+
+    const newUser = new User({
+      name: "Shweta",
+      email: req.body.email,
+      password: req.body.password
+    })
+
+    await newUser.save()
+
+    res.json({
+      message: "User saved in database"
+    })
+
+  } catch (error) {
+
+    console.log(error)
+
+    res.status(500).json({
+      message: "Server error"
+    })
+  }
+
 })
 
 app.listen(5000, () => {
